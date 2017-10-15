77bc96795e69c13f86ecd68f4a7289b75dc19c24
Dongho_Nam <sirius0916@gmail.com>
2017-10-15 18:59:27 +0900

    modify README.md file for assignment

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          공개소프트웨어 실습 첫 번째 과제 201302394 남동호
+        </p>
       </div>
     );
   }
